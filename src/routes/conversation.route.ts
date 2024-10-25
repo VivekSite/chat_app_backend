20089064e60b03b4766cc28e33571ecacdb23aa0
VivekSite <sram39266@gmail.com>
2024-10-25 09:48:25 +0530

    Remove message routes as it is not needed

--- src/routes/conversation.route.ts
+++ src/routes/conversation.route.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import { createConversationHandler, getConversationHandler } from "../controllers";
 import { ValidateCreateConversation } from "../middlewares/conversation.middleware";
-import messageRoutes from "./message.route";
 
 const app = Router({
 	mergeParams: true
@@ -9,6 +8,5 @@ const app = Router({
 
 app.post("/", ValidateCreateConversation, createConversationHandler);
 app.get("/:userId", getConversationHandler);
-app.use("/:conversationId", messageRoutes);
 
 export default app;
